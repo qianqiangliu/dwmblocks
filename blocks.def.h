7974f7a7c099b7b57e45caad0f712a989a763767
Qianqiang Liu <qianqiang.liu@163.com>
2025-05-18 21:30:35 +0800

    Add new status
    Signed-off-by: Qianqiang Liu <qianqiang.liu@163.com>

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"  ",	"~/.dwm/wifi",			0,		7},
+	{" ",	"~/.dwm/bluetooth",		5,		8},
+	{" ",	"~/.dwm/cpu-temp",	        5,		9},
+	{"",	"~/.dwm/battery",	        60,		10},
+	{"",	"~/.dwm/volume",		1,		11},
+	{" ",	"date '+%D %H:%M '",		60,		12},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
